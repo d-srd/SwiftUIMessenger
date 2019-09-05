0654bf64a31cacd313c80fc7d86ec1ea3b60be6c
Dino Srdoč <dino.srdoc@bornfight.com>
2019-09-05 13:49:25 +0200

    Initial Commit

---
+++ WhatsAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsAppSwiftUI
+//
+//  Created by Dino on 05/09/2019.
+//  Copyright © 2019 Dino Srdoc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
