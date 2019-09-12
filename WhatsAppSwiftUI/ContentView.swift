1aa8ec08df3125b1444fc43a67f04bfa19d16811
Dino Srdoč <dino.srdoc@bornfight.com>
2019-09-12 10:59:10 +0200

    the messages make more sense now

--- WhatsAppSwiftUI/ContentView.swift
+++ WhatsAppSwiftUI/ContentView.swift
@@ -53,68 +53,23 @@ let allMessagesString = """
 [
     {
         "id": 0,
-        "content": "Foo bar",
+        "content": "Hi there John!",
         "sender": "me"
     },
     {
         "id": 1,
-        "content": "Foo bar? FOOOOOO! Bar?!?!",
-        "sender": "Pero"
+        "content": "Hello.",
+        "sender": "John"
     },
     {
         "id": 2,
-        "content": "Baz quuz. Foo. Bar foo bar bar foo quux. Foo bar. Bar?",
+        "content": "What's for dinner tomorrow? I've been thinking about making some triple layered apple pie with the Golden Smiths that Jonathan has given us.",
         "sender": "me"
     },
     {
         "id": 3,
-        "content": "Bar bar bar bar bar. Foo! Bar? Foo bar foo foo quux. Foo. Foobar.",
-        "sender": "Pero"
-    },
-    {
-        "id": 4,
-        "content": "bar bar bar...",
-        "sender": "me"
-    },
-    {
-        "id": 5,
-        "content": "bar bar bar....... Fooooo",
-        "sender": "Pero"
-    },
-    {
-        "id": 6,
-        "content": "Foobar bar foo bar?",
-        "sender": "me"
-    },
-    {
-        "id": 7,
-        "content": "fooooofoooooooo",
-        "sender": "me"
-    },
-    {
-        "id": 8,
-        "content": "quux bar bax xab rab fooo foo oooooooooo",
-        "sender": "me"
-    },
-    {
-        "id": 9,
-        "content": "Foo. Foo foo foo.",
-        "sender": "Pero"
-    },
-    {
-        "id": 10,
-        "content": "Foobar quux foobar foo bar bar bar foo.",
-        "sender": "me"
-    },
-    {
-        "id": 11,
-        "content": "bar bar foo",
-        "sender": "Pero"
-    },
-    {
-        "id": 12,
-        "content": "foo",
-        "sender": "me"
+        "content": "How about some fried apples?",
+        "sender": "John"
     }
 ]
 
